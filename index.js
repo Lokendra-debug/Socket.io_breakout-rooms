18438ee613e9484dab3fca65324c7f781575356c
lokendra <thelokendrasinghrathore@gmail.com>
2023-05-25 12:23:24 +0530

    ok

--- index.js
+++ index.js
@@ -5,11 +5,21 @@ const server=http.createServer(app)
 const {Server}=require(`socket.io`)
 const io=new Server(server)
 
+let rooms=[{"Room 1":0},{"Room 2":0},{"Room 3":0}]
+
 io.on('connection', (socket) => {
   console.log('a user connected');
+  let room=rooms[Math.floor(Math.random()*3)]
+  let roomName=Object.keys(room)[0]
+  socket.join(roomName);
+  room[roomName]+=1
   socket.on('disconnect', () => {
+    room[roomName]-=1
     console.log('a user disconnected');
+    io.sockets.in(roomName).emit(`userCount`,`Total users: ${room[roomName]}`)
   });
+  socket.send(`you are in ${roomName}`)
+  io.sockets.in(roomName).emit(`userCount`,`Total users: ${room[roomName]}`)
 });
 
 
